7661d7905e51ef51030601f5cfdaae85cff64f7d
77web <info@77-web.com>
2021-12-19 23:12:48 +0900

    TaskFactory::createOne(), createMany()を使ってみる

--- tests/Controller/TaskController/CompletedTest.php
+++ tests/Controller/TaskController/CompletedTest.php
@@ -3,16 +3,23 @@ declare(strict_types=1);
 
 namespace App\Tests\Controller\TaskController;
 
+use App\Tests\Factory\TaskFactory;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class CompletedTest extends WebTestCase
 {
     public function test()
     {
+        // 1件未完了タスクを作る
+        TaskFactory::createOne();
+        // 2件完了済タスクを作る
+        TaskFactory::createMany(2, ['completedAt' => new \DateTimeImmutable('yesterday')]);
+
         $client = static::createClient();
         $crawler = $client->request('GET', '/task/completed');
 
         $this->assertResponseIsSuccessful();
         $this->assertTrue($crawler->filter('title')->text() === '完了したタスク');
+        $this->assertTrue($crawler->filter('li')->count() === 2, '完了済の2件のみ');
     }
 }
